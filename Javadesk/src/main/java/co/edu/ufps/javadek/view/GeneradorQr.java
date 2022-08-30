dbc6a84555c8bc47b4b106207a40aac37a0b4a21
JuanBM16 <101369022+JuanBM16@users.noreply.github.com>
2022-08-30 16:48:53 -0500

    Genera Qr

--- Javadesk/src/main/java/co/edu/ufps/javadek/view/GeneradorQr.java
+++ Javadesk/src/main/java/co/edu/ufps/javadek/view/GeneradorQr.java
@@ -4,6 +4,23 @@
  */
 package co.edu.ufps.javadek.view;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author usuario
@@ -23,19 +40,19 @@ public class GeneradorQr extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        panelRound1 = new co.edu.ufps.javadek.view.PanelRound();
-        sitiWeb1 = new javax.swing.JLabel();
-        ExportarBut1 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
         panelRound2 = new co.edu.ufps.javadek.view.PanelRound();
         sitiWeb2 = new javax.swing.JLabel();
+        GenerarBut1 = new javax.swing.JButton();
+        textUrl = new javax.swing.JTextField();
+        sitiWeb1 = new javax.swing.JLabel();
         ExportarBut2 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        labelQr = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
         userNombre = new javax.swing.JLabel();
         icono = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(132, 140, 220));
@@ -45,56 +62,7 @@ public class GeneradorQr extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Genere su código QR");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 100, 280, -1));
-
-        panelRound1.setBackground(new java.awt.Color(255, 255, 255));
-        panelRound1.setRoundBottomLeft(10);
-        panelRound1.setRoundBottomRight(10);
-        panelRound1.setRoundTopLeft(10);
-        panelRound1.setRoundTopRight(10);
-
-        sitiWeb1.setBackground(new java.awt.Color(0, 0, 0));
-        sitiWeb1.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
-        sitiWeb1.setText("Código QR");
-
-        ExportarBut1.setBackground(new java.awt.Color(105, 115, 184));
-        ExportarBut1.setFont(new java.awt.Font("Bell MT", 1, 14)); // NOI18N
-        ExportarBut1.setForeground(new java.awt.Color(255, 255, 255));
-        ExportarBut1.setText("Exportar QR");
-        ExportarBut1.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        ExportarBut1.setBorderPainted(false);
-
-        javax.swing.GroupLayout panelRound1Layout = new javax.swing.GroupLayout(panelRound1);
-        panelRound1.setLayout(panelRound1Layout);
-        panelRound1Layout.setHorizontalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(ExportarBut1)
-                .addGap(76, 76, 76))
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addGroup(panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panelRound1Layout.createSequentialGroup()
-                        .addGap(43, 43, 43)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(panelRound1Layout.createSequentialGroup()
-                        .addGap(73, 73, 73)
-                        .addComponent(sitiWeb1)))
-                .addContainerGap(52, Short.MAX_VALUE))
-        );
-        panelRound1Layout.setVerticalGroup(
-            panelRound1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRound1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(sitiWeb1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 188, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(ExportarBut1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(14, 14, 14))
-        );
-
-        jPanel1.add(panelRound1, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 170, 259, 270));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, 280, -1));
 
         panelRound2.setBackground(new java.awt.Color(255, 255, 255));
         panelRound2.setRoundBottomLeft(10);
@@ -106,56 +74,86 @@ public class GeneradorQr extends javax.swing.JFrame {
         sitiWeb2.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
         sitiWeb2.setText("Sitio web (URL)");
 
-        ExportarBut2.setBackground(new java.awt.Color(105, 115, 184));
-        ExportarBut2.setFont(new java.awt.Font("Bell MT", 1, 14)); // NOI18N
-        ExportarBut2.setForeground(new java.awt.Color(255, 255, 255));
-        ExportarBut2.setText("Generar código QR");
-        ExportarBut2.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        ExportarBut2.setBorderPainted(false);
-        ExportarBut2.addActionListener(new java.awt.event.ActionListener() {
+        GenerarBut1.setBackground(new java.awt.Color(105, 115, 184));
+        GenerarBut1.setFont(new java.awt.Font("Bell MT", 1, 14)); // NOI18N
+        GenerarBut1.setForeground(new java.awt.Color(255, 255, 255));
+        GenerarBut1.setText("Generar código QR");
+        GenerarBut1.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        GenerarBut1.setBorderPainted(false);
+        GenerarBut1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ExportarBut2ActionPerformed(evt);
+                GenerarBut1ActionPerformed(evt);
             }
         });
 
-        jTextField1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 12)); // NOI18N
-        jTextField1.setText("URL...");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        textUrl.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 12)); // NOI18N
+        textUrl.setText("URL...");
+        textUrl.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                textUrlActionPerformed(evt);
             }
         });
+
+        sitiWeb1.setBackground(new java.awt.Color(0, 0, 0));
+        sitiWeb1.setFont(new java.awt.Font("Bell MT", 1, 18)); // NOI18N
+        sitiWeb1.setText("Código QR");
+
+        ExportarBut2.setBackground(new java.awt.Color(105, 115, 184));
+        ExportarBut2.setFont(new java.awt.Font("Bell MT", 1, 14)); // NOI18N
+        ExportarBut2.setForeground(new java.awt.Color(255, 255, 255));
+        ExportarBut2.setText("Exportar QR");
+        ExportarBut2.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        ExportarBut2.setBorderPainted(false);
 
         javax.swing.GroupLayout panelRound2Layout = new javax.swing.GroupLayout(panelRound2);
         panelRound2.setLayout(panelRound2Layout);
         panelRound2Layout.setHorizontalGroup(
             panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound2Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(sitiWeb2, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(sitiWeb1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(92, 92, 92))
             .addGroup(panelRound2Layout.createSequentialGroup()
                 .addGroup(panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(panelRound2Layout.createSequentialGroup()
-                        .addGap(24, 24, 24)
-                        .addComponent(sitiWeb2, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(107, 107, 107)
+                        .addComponent(GenerarBut1))
                     .addGroup(panelRound2Layout.createSequentialGroup()
-                        .addGap(69, 69, 69)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 329, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(panelRound2Layout.createSequentialGroup()
-                        .addGap(142, 142, 142)
-                        .addComponent(ExportarBut2)))
-                .addContainerGap(52, Short.MAX_VALUE))
+                        .addGap(36, 36, 36)
+                        .addComponent(textUrl, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 120, Short.MAX_VALUE)
+                .addComponent(labelQr, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(49, 49, 49))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelRound2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(ExportarBut2)
+                .addGap(92, 92, 92))
         );
         panelRound2Layout.setVerticalGroup(
             panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelRound2Layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addComponent(sitiWeb2)
-                .addGap(58, 58, 58)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 86, Short.MAX_VALUE)
-                .addComponent(ExportarBut2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(25, 25, 25))
+                .addGroup(panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelRound2Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(panelRound2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(sitiWeb2)
+                            .addComponent(sitiWeb1))
+                        .addGap(58, 58, 58)
+                        .addComponent(textUrl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                        .addComponent(GenerarBut1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(36, 36, 36))
+                    .addGroup(panelRound2Layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(labelQr, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(ExportarBut2, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        jPanel1.add(panelRound2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 170, 450, 260));
+        jPanel1.add(panelRound2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, 700, 260));
 
         jPanel4.setBackground(new java.awt.Color(175, 179, 210));
 
@@ -169,39 +167,89 @@ public class GeneradorQr extends javax.swing.JFrame {
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                .addGap(0, 743, Short.MAX_VALUE)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(userNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(icono))
-                .addGap(29, 29, 29))
+                .addGap(0, 667, Short.MAX_VALUE)
+                .addComponent(userNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(icono)
+                .addGap(87, 87, 87))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(icono)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(userNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(userNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(icono))
+                .addContainerGap(9, Short.MAX_VALUE))
         );
 
         jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 830, 40));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 490));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 760, 490));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void textUrlActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textUrlActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_textUrlActionPerformed
 
-    private void ExportarBut2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExportarBut2ActionPerformed
+    private void GenerarBut1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GenerarBut1ActionPerformed
         // TODO add your handling code here:
+     int size = 1000;
+        String FileType = "png";
+
+        String codigo = textUrl.getText().trim();
+        
+        // Ruta de la imagen
         String filePath = "";
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            filePath = chooser.getSelectedFile().getAbsolutePath();
+        }
+
+       
+        UUID uuid = UUID.randomUUID();
+        String randonName = uuid.toString();
+
+        // Generar codigo Qr 
+        QRCodeWriter qrcode = new QRCodeWriter();
+        try {
+            BitMatrix matrix = qrcode.encode(codigo, BarcodeFormat.QR_CODE, size, size);
+            File f = new File(filePath + "/" + randonName + "." + FileType);
+            int matrixWidth = matrix.getWidth();
+            BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+            image.createGraphics();
+
+            Graphics2D gd = (Graphics2D) image.getGraphics();
+            gd.setColor(Color.WHITE); 
+            gd.fillRect(0, 0, size, size);
+            gd.setColor(Color.black); 
+
+            for (int b = 0; b < matrixWidth; b++) {
+                for (int j = 0; j < matrixWidth; j++) {
+                    if (matrix.get(b, j)) {
+                        gd.fillRect(b, j, 1, 1);
+                    }
+                }
+            }
+            
+            // Mostrar imagen Qr en el label
+            
+            ImageIO.write(image, FileType, f);
+            Image mImagen = new ImageIcon(filePath + "/" + randonName + "." + FileType).getImage();
+            ImageIcon mIcono = new ImageIcon(mImagen.getScaledInstance(labelQr.getWidth(), labelQr.getHeight(), 0));
+            labelQr.setIcon(mIcono);
+            
+        } catch (WriterException ex) {
+            Logger.getLogger(GeneradorQr.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(GeneradorQr.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
-    }//GEN-LAST:event_ExportarBut2ActionPerformed
+    }//GEN-LAST:event_GenerarBut1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -239,18 +287,17 @@ public class GeneradorQr extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton ExportarBut1;
     private javax.swing.JButton ExportarBut2;
+    private javax.swing.JButton GenerarBut1;
     private javax.swing.JLabel icono;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JTextField jTextField1;
-    private co.edu.ufps.javadek.view.PanelRound panelRound1;
+    private javax.swing.JLabel labelQr;
     private co.edu.ufps.javadek.view.PanelRound panelRound2;
     private javax.swing.JLabel sitiWeb1;
     private javax.swing.JLabel sitiWeb2;
+    private javax.swing.JTextField textUrl;
     private javax.swing.JLabel userNombre;
     // End of variables declaration//GEN-END:variables
 }
